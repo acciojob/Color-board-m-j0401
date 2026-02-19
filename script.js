26129006c1105897967f48875b80bc7e7da82b74
m-j0401 <mehul.jain2206@gmail.com>
2026-02-19 19:09:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let a = 800;
+let parent = document.querySelector(".container");
+
+const dFrag = document.createDocumentFragment();
+
+for (let i = 0; i < a; i++) {
+    let d = document.createElement("div");
+    d.className = "square";
+    dFrag.appendChild(d);
+}
+
+parent.appendChild(dFrag);
+
