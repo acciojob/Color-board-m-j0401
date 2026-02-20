2e36a7b7908807f449d8ff4d298196438439c5a1
m-j0401 <mehul.jain2206@gmail.com>
2026-02-20 19:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let a = 800;
 let parent = document.querySelector(".container");
 
+
 const dFrag = document.createDocumentFragment();
 
 for (let i = 0; i < a; i++) {
@@ -10,6 +11,25 @@ for (let i = 0; i < a; i++) {
 }
 
 parent.appendChild(dFrag);
+let box=document.querySelectorAll(".square");
+
+for(let i=0;i<box.length;i++)
+	{
+box[i].addEventListener("mouseover",changeColor);
+	}
+function changeColor(e) {
+	//if (!e.target.classList.contains("square")) return;
+	let t=e.target;
+	let r=Math.floor(Math.random()*256);
+	let g=Math.floor(Math.random()*256);
+	let b=Math.floor(Math.random()*256);
+	t.style.backgroundColor=`rgb(${r},${g},${b})`;
+	setTimeout(() => {
+    t.style.backgroundColor = `rgb(${29},${29},${29})`;
+}, 1000);
+	
+}
+
 
 
 
